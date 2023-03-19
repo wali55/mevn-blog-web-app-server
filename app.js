c8d38e136ada9b723fe9ff3c3f805196cc74a65a
wali55 <walisantunu@gmail.com>
2023-03-19 23:46:42 +0600

    complete web app

---
+++ app.js
@@ -0,0 +1,35 @@
+// imports
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('uploads'));
+
+// database connection
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => console.log('connected to the database'))
+  .catch((err) => console.log(err));
+
+// routes prefix
+app.use('/api/post', require('./routes/routes'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(__dirname + '/dist/'));
+  app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/dist/index.html');
+  });
+}
+
+// start server
+app.listen(port, () =>
+  console.log(`server running at http://localhost:${port}`)
+);
---
+++ dist/js/about.b1a198de.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkclient"]=self["webpackChunkclient"]||[]).push([[443],{2471:function(n,u,e){e.r(u),e.d(u,{default:function(){return f}});var t=function(){var n=this,u=n._self._c;return u("h1",[n._v("About Page")])},l=[],c=e(1001),r={},s=(0,c.Z)(r,t,l,!1,null,null,null),f=s.exports}}]);
+//# sourceMappingURL=about.b1a198de.js.map
